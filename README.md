f4317f87c84815a57501c73d3ced910bd3a81bfc
zxygentoo <zxy@matters.news>
2022-10-13 09:11:04 +0000

    update code, tests and README.md

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
-# CJieba
+# CJieba.jl
+
+[CJieba](https://github.com/yanyiwu/cjieba/) word tokenizor and part-of-speech tagging in Julia.
+
+_CJieba is nice and simple and quite adequate for simple tasks specially those prioritize speed over accuracy._
+
+
+## Usage
+
+```julia
+using CJieba
+s = "他来到了网易杭研大厦"
+```
+
+Tokenize sentence:
+
+```julia
+cut(s)
+```
+
+Output:
+
+```
+6-element Vector{String}:
+ "他"
+ "来到"
+ "了"
+ "网易"
+ "杭研"
+ "大厦"
+```
+
+Tokenize and POS tagging:
+
+```julia
+tag(s)
+```
+
+Output:
+
+```
+6-element Vector{Tuple{String, String}}:
+ ("他", "r")
+ ("来到", "v")
+ ("了", "ul")
+ ("网易", "n")
+ ("杭研", "x")
+ ("大厦", "n")
+```
+
+Tokenize without tag:
+
+```julia
+cut(s; without="x")
+```
+
+Output:
+
+```
+5-element Vector{String}:
+ "他"
+ "来到"
+ "了"
+ "网易"
+ "大厦"
+```
+
+Manually create and free handle:
+
+```julia
+jieba = JIEBA()
+add_word!(jieba, "网易杭研大厦")
+cut(jieba, s)
+tag(jieba, s)
+free!(jieba)
+```
+
+Output:
+
+```
+4-element Vector{String}:
+ "他"
+ "来到"
+ "了"
+ "网易杭研大厦"
+
+4-element Vector{Tuple{String, String}}:
+ ("他", "r")
+ ("来到", "v")
+ ("了", "ul")
+ ("网易杭研大厦", "u")
+```
+
+This works too:
+
+```julia
+jieba = JIEBA(; user_words=["网易杭研大厦"])
+```
+
+Or pass a file path:
+
+```julia
+jieba = JIEBA(; user_words="/path/to/your/user_dict")
+```
+
+```do``` syntax (*auto free handle*):
+
+```julia
+JIEBA(; user_words=["网易杭研大厦"]) do jieba
+   tag(jieba, s)
+end
+```
+
+## Goals
+
+- Make it idiomatic to use in Julia.
+- Make it safe, *mostly (I'm pretty sure some weird ill-formated user_dict can still segfault it...)* 
+- Keep it simple (didn't even expose keyword extractor).
--- src/CJieba.jl
+++ src/CJieba.jl
@@ -1,5 +1,133 @@
 module CJieba
 
-# Write your package code here.
+using CJieba_jll
 
+export JIEBA, add_word!, cut, tag, free!
+
+
+mutable struct Jieba
+    const ptr::Ptr{Cvoid}
+    freed::Bool
 end
+
+
+struct CJiebaWord
+    word::Cstring
+    len::Csize_t
+end
+
+
+function create_jieba(user_words)
+    Jieba(
+        ccall((:NewJieba, libcjieba),
+            Ptr{Cvoid},
+            (Cstring, Cstring, Cstring, Cstring, Cstring),
+            jieba_dict, hmm_model, user_words, idf, stop_words),
+        false
+    )
+end
+
+
+function JIEBA(; user_words::Union{String, Vector{String}} = user_dict)::Jieba
+    if isa(user_words, String)
+        if isfile(user_words)
+            create_jieba(user_words)
+        else
+            throw(ArgumentError("$(user_words) not found."))
+        end
+    else
+        jieba = create_jieba(user_dict)
+        for w in user_words add_word!(jieba, w) end
+        jieba
+    end
+end
+
+
+function JIEBA(f::Function; user_words = user_dict)
+    jieba = JIEBA(; user_words = user_words)
+    try f(jieba) finally free!(jieba) end
+end
+
+
+function add_word!(jieba::Jieba, word::String)::Bool
+    ccall((:JiebaInsertUserWord, libcjieba),
+        Cuchar, (Ptr{Cvoid}, Cstring), jieba.ptr, word)
+end
+
+
+function cut(
+    jieba::Jieba, s::String; without::Union{String, Nothing} = nothing
+)::Vector{String}
+
+    !jieba.freed || throw(ArgumentError("Jieba already freed."))
+
+    ws = without == nothing ?
+        ccall((:Cut, libcjieba),
+            Ptr{CJiebaWord}, (Ptr{Cvoid}, Cstring, Csize_t),
+            jieba.ptr, s, sizeof(s)) :
+        ccall((:CutWithoutTagName, libcjieba),
+            Ptr{CJiebaWord}, (Ptr{Cvoid}, Cstring, Csize_t, Cstring),
+            jieba.ptr, s, sizeof(s), without)
+
+    res, i = Vector{String}(), 1
+    while true
+        w = unsafe_load(ws, i).word
+        if w == C_NULL break end
+        push!(res, unsafe_string(pointer(w), unsafe_load(ws, i).len))
+        i += 1
+    end
+
+    ccall((:FreeWords, libcjieba), Cvoid, (Ptr{CJiebaWord},), ws)
+
+    res
+end
+
+
+function cut(
+    s::String; without::Union{String, Nothing} = nothing
+)::Vector{String}
+    JIEBA() do jieba cut(jieba, s; without = without) end
+end
+
+
+function tag(jieba::Jieba, s::String)::Vector{Tuple{String, String}}
+    !jieba.freed || throw(ArgumentError("Jieba already freed."))
+
+    wts = ccall((:CutWithTag, libcjieba),
+        Ptr{Cvoid},
+        (Ptr{Cvoid}, Cstring, Csize_t),
+        jieba.ptr, s, sizeof(s))
+
+    res, p = Vector{Tuple{String, String}}(), wts
+
+    # CJiebaWordWithTag is a C99-compliant variable length struct:
+    # https://github.com/yanyiwu/cjieba/blob/1db462d33255aff08802c248b6d6e59202b5619e/lib/jieba.h#L16
+    # we directly calculate offsets here as doc suggested.
+    while true
+        wptr = unsafe_load(Ptr{Cstring}(p))
+        if wptr == C_NULL break end
+        tptr = p + sizeof(Cstring) + sizeof(Csize_t)
+        t = unsafe_string(Ptr{Int8}(tptr))
+        len = unsafe_load(Ptr{Csize_t}(p + sizeof(Csize_t)))
+        w = unsafe_string(pointer(wptr), len)
+        push!(res, (w, t))
+        p += sizeof(Cstring) + sizeof(Csize_t) + sizeof(t) + 1
+    end
+
+    ccall((:FreeWordTag, libcjieba), Cvoid, (Ptr{Cvoid},), wts)
+    res
+end
+
+
+function tag(s::String)::Vector{Tuple{String, String}}
+    JIEBA() do jieba tag(jieba, s) end
+end
+
+
+function free!(jieba::Jieba)
+    ccall((:FreeJieba, libcjieba), Cvoid, (Ptr{Cvoid},), jieba.ptr)
+    jieba.freed = true
+end
+
+
+end # module
